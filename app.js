4330460647474bcb268ecc5b6aaf4866e422b836
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-13 12:46:39 +0900

    서버 작업 시작

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-// 서버 생성
-// 새로
+const http = require('http');
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const contentType = {
+//     'Content-Type': 'text/html;charset=utf-8',
+// };
+// http.createServer((req, res) => {
+//     if (req.method === 'GET' && req.url === '/') {
+//         res.writeHead(200, contentType);
+//         fs.readFile('index.html', (err, data) => {
+//             if (err) {
+//                 console.log('파일 호출 에러');
+//             } else {
+//                 res.end(data);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, contentType);
+//         res.end('<h1>요청 페이지를 찾을 수 없음</h1>');
+//     }
+// }).listen(8080);
