67e5cec03b0bafbd116bc74eb76d44f3bc1c7e7f
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-13 14:42:18 +0900

    파일을 불러올 수 없을때  '파일 호출 에러'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { error } = require('console');
+
 const http = require('http');
-const fs = require('http');
+const fs = require('fs');
 const contentType = {
   'content-Type' : 'text/html',
   'charset' : 'utf-8',
@@ -10,28 +10,14 @@ http.createServer((request, response) => {
     response.writeHead(200, contentType);
     fs.readFile('index.html', (err, data) => {
       if(err) {
-        console.error('파일 호출 에러')
+        console.log('파일 호출 에러')
       } else {
         response.end(data);
       }
 
-    })
-  }
-})
-
-
-// http.createServer((req, res) => {
-//     if (req.method === 'GET' && req.url === '/') {
-//         res.writeHead(200, contentType);
-//         fs.readFile('index.html', (err, data) => {
-//             if (err) {
-//                 console.log('파일 호출 에러');
-//             } else {
-//                 res.end(data);
-//             }
-//         });
-//     } else {
-//         res.writeHead(404, contentType);
-//         res.end('<h1>요청 페이지를 찾을 수 없음</h1>');
-//     }
-// }).listen(8080);
+    });
+  // } else {
+  //   response.writeHead(404, contentType);
+  //   response.end('<h1>요청 페이지를 찾을 수 없음</h1>');
+  // }
+}).listen(8080);
